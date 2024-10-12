9a7a12d99d1d402f5ae3406a0c5d4d230880dad8
= <yashbansodgit@gmail.com>
2024-10-12 16:22:07 +0530

    feat: logs for better debug users controller

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createError from "http-errors";
 import JsonWebToken from "jsonwebtoken";
-import UserService from "@/services/user.service";
+import UserService from "@/services/users.service";
 import TokensService from "@/services/tokens.service";
 import { CreateUserDto } from "@/dto/users";
 import { ForgotPasswordDto, ResetPasswordDto } from "@/dto/autentication";
@@ -19,7 +19,7 @@ class AutenticationController {
   async register(req: Request, res: Response, next: NextFunction) {
     const { email, ...rest } = req.body as CreateUserDto;
     this.logger.debug(`initiate registering user ${email}`);
-    const userExists = await this.userService.findOne({ email });
+    const userExists = await this.userService.findOne({ where: { email } });
     if (userExists) {
       this.logger.debug(`user already exists with ${email} email`);
       return next(createError.Conflict("user already exists"));
@@ -34,15 +34,17 @@ class AutenticationController {
     const { email, password } = req.body as CreateUserDto;
     this.logger.debug(`initiate login user ${email}`);
     const user = await this.userService.findOne({
-      email,
+      where: { email },
     });
 
     if (!user) {
+      this.logger.debug("user not found");
       return next(createError.NotFound("user not found"));
     }
 
     this.logger.debug("verifying password");
     if (user.password !== password) {
+      this.logger.debug("invalid credentials");
       return next(createError.UnprocessableEntity("invalid credentials"));
     }
 
@@ -59,21 +61,27 @@ class AutenticationController {
     this.logger.debug("login user successfully");
     return res.json({ accessToken });
   }
+
   async profile(req: Request, res: Response, next: NextFunction) {
     const id = (req as AuthenticatedRequest).user.sub;
-    const user = await this.userService.findOne({ id });
+    const user = await this.userService.findOne({ where: { id: Number(id) } });
     if (!user) {
+      this.logger.debug("user not found");
       return next(createError.NotFound("user not found"));
     }
+    this.logger.debug("profile user successfully");
     return res.json(user);
   }
 
   async forgot(req: Request, res: Response, next: NextFunction) {
     const { email } = req.body as ForgotPasswordDto;
-    const user = await this.userService.findOne({ email });
+    this.logger.debug(`initiate forgot password process for ${email}`);
+    const user = await this.userService.findOne({ where: { email } });
     if (!user) {
+      this.logger.debug("user not found");
       return next(createError.NotFound("user not found"));
     }
+
     const payload: JsonWebToken.JwtPayload = {
       sub: String(user.id),
       role: user.role,
@@ -82,29 +90,37 @@ class AutenticationController {
     const tokenOptions: JsonWebToken.SignOptions = {
       expiresIn: "10m",
     };
+    this.logger.debug("generating forgot password token");
     const token = this.forgotPasswordTokensService.sign(payload, tokenOptions);
 
+    this.logger.debug("forgot password process completed successfully");
     return res.json({ token });
   }
 
   async reset(req: Request, res: Response, next: NextFunction) {
     const { token } = req.params;
+    this.logger.debug(`initiate reset password process for ${token}`);
     const match = this.forgotPasswordTokensService.verify(token);
     if (!match) {
+      this.logger.debug("invalid token");
       return next(createError.InternalServerError());
     }
     const { email } = match as JsonWebToken.JwtPayload;
 
     const userExists = await this.userService.findOne({
-      email: email as string,
+      where: {
+        email: email as string,
+      },
     });
 
     if (!userExists) {
+      this.logger.debug("user not found");
       return next(createError.NotFound("user not found"));
     }
 
     const { password } = req.body as ResetPasswordDto;
 
+    this.logger.debug("updating user password");
     const user = await this.userService.update(
       { email: email as string },
       {
@@ -113,9 +129,11 @@ class AutenticationController {
     );
 
     if (!user) {
+      this.logger.debug("user update failed");
       return next(createError.InternalServerError());
     }
 
+    this.logger.debug("user password updated successfully");
     return res.json(user);
   }
 }
---
+++ src/services/users.service.ts
@@ -0,0 +1,40 @@
+import { User } from "@/entity/User";
+import {
+  DeepPartial,
+  DeleteResult,
+  FindManyOptions,
+  FindOneOptions,
+  FindOptionsWhere,
+  Repository,
+  SaveOptions,
+  UpdateResult,
+} from "typeorm";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+
+class UsersService {
+  constructor(private readonly usersRepository: Repository<User>) {}
+  async create(createUserDto: DeepPartial<User>, options?: SaveOptions) {
+    return await this.usersRepository.save(createUserDto, options);
+  }
+
+  findAll(options?: FindManyOptions<User> | undefined): Promise<User[]> {
+    return this.usersRepository.find(options);
+  }
+
+  findOne(options: FindOneOptions<User>): Promise<User | null> {
+    return this.usersRepository.findOne(options);
+  }
+
+  update(
+    expression: Record<string, string>,
+    updateUserDto: QueryDeepPartialEntity<User>,
+  ): Promise<UpdateResult> {
+    return this.usersRepository.update(expression, updateUserDto);
+  }
+
+  delete(criteria: FindOptionsWhere<User>): Promise<DeleteResult> {
+    return this.usersRepository.delete(criteria);
+  }
+}
+
+export default UsersService;
