e89e5def90a89e89efd1655fa8a7273135e839a6
= <yashbansodgit@gmail.com>
2024-12-31 23:19:42 +0530

    feat: integrated commit-lint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import express, { Express } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
 import usersRouter from "@/routes/users.router.js";
 import authRouter from "@/routes/auth.router.js";
 import quizzesRouter from "@/routes/quizzes.router.js";
@@ -9,14 +10,15 @@ import questionsRouter from "@/routes/questions.router.js";
 import answersRouter from "@/routes/answers.router.js";
 import submissionsRouter from "@/routes/submissions.router.js";
 import resultsRouter from "@/routes/results.router.js";
+import configuration from "@/config/configuration.js";
 
 import globalErrorHandler from "@/middlewares/error-handler.js";
 
 export const createServer = (): Express => {
   const app = express();
   app
-
     .use(helmet({ contentSecurityPolicy: false }))
+    .use(cors({ origin: configuration.domain }))
     .use(morgan("dev"))
     .use(express.json())
     .use(express.static("public"))
