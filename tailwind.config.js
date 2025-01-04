0748cf79186a8568a5631cbcd1ef1c21c49666b8
Yash Gajanan Bansod <90038425+yash-bansod-2003@users.noreply.github.com>
2025-01-05 00:08:50 +0530

    Master (#2)
    * fix: prettir command not found
    
    * fix: referenceError: require is not defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssanimate from "tailwindcss-animate";
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -63,5 +65,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssanimate],
 }
